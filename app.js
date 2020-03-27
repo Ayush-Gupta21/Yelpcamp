b778080b05660aba9bf1da1cb4687039c4ba2e72
Ayush-Gupta21 <54956547+Ayush-Gupta21@users.noreply.github.com>
2020-03-27 16:50:34 +0530

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ var campgroundRoutes = require("./routes/campgrounds"),
 	commentRoutes    = require("./routes/comments"),
 	indexRoutes      = require("./routes/index");
 
-mongoose.connect("mongodb+srv://Ayush-Gupta21:ayush@cluster0-0w3w5.mongodb.net/test?retryWrites=true&w=majority");
+mongoose.connect("process.env.DATABASEURL");
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
 app.use(express.static(__dirname+"/public"));
